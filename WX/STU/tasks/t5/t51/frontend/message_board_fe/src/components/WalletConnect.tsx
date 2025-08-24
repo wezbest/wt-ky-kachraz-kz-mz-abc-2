de121ce5bc30713b197a4370e51c5ff6bcb65779
wezbest <110793713+wezbest@users.noreply.github.com>
2025-08-24 14:29:49 +0000

    🫦

--- WX/STU/tasks/t5/t51/frontend/message_board_fe/src/components/WalletConnect.tsx
+++
@@ -1,9 +0,0 @@
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui"
-
-export default function WalletConnect() {
-  return (
-    <div>
-      <WalletMultiButton className="btn" />
-    </div>
-  )
-}
