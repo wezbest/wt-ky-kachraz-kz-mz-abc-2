50b335f015336eef7f83aa9593a49c7618de6d2b
wezbest <110793713+wezbest@users.noreply.github.com>
2025-08-24 16:33:58 +0000

    🫦

---
+++ WX/STU/tasks/t5/t51/anchor_project/message_board/runz.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# Commands for running
+
+# === Config ===
+WALLET_DIR="wallets"   # <-- all wallets stored here
+WALLETS=("wallet2.json")  # <-- list of wallets you want to manage
+
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+NC='\033[0m' # No Color
+
+# Header function
+hea1() {
+    echo -e "${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
+    echo -e "${PURPLE}$1${NC}"
+    echo -e "${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
+}
+
+# Running the tests 
+run_test() {
+    hea1 "Anchor test"
+    co1="anchor test"
+    echo -e "${GREEN}$co1$NC"
+    eval "$co1"
+}
+
+# Execution zone 
+
+# Running the tests 
+run_test
